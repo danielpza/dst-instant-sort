b68043a790eaaa9f61def46e9b4da0e0aecf860c
Daniel Perez <danielpza@protonmail.com>
2026-08-31 05:33:08 -0700

    fix: control keybindings not working when original slot in 11-15

--- modmain.lua
+++ modmain.lua
@@ -236,13 +236,37 @@ AddClassPostConstruct("widgets/containerwidget", function(self)
 end)
 
 AddClassPostConstruct("screens/playerhud", function(self)
+   local REGION_1_START = GLOBAL.CONTROL_INV_1
+   local REGION_1_END = GLOBAL.CONTROL_INV_10
+   local REGION_1_OFFSET = GLOBAL.CONTROL_INV_1 - 1
+
+   local REGION_2_START = GLOBAL.CONTROL_INV_11
+   local REGION_2_END = GLOBAL.CONTROL_INV_15
+   local REGION_2_OFFSET = GLOBAL.CONTROL_INV_11 - 1
+
+   local CONTROL_BORDER = GLOBAL.CONTROL_INV_10 - REGION_1_OFFSET
+
    local OnControl = self.OnControl
    function self:OnControl(control, down)
       -- intercept control
-      if player_virtual_inventory and control >= GLOBAL.CONTROL_INV_1 and control <= GLOBAL.CONTROL_INV_10 then
-         local offset = GLOBAL.CONTROL_INV_1 - 1
-         local hot_key_num = control - offset
-         control = player_virtual_inventory:GetOriginalSlot(hot_key_num) + offset
+      if player_virtual_inventory and control >= REGION_1_START and control <= REGION_1_END then
+         local hot_key_num = control - REGION_1_OFFSET
+         local new_hot_key_num = player_virtual_inventory:GetOriginalSlot(hot_key_num)
+
+         if new_hot_key_num <= CONTROL_BORDER then
+            control = new_hot_key_num + REGION_1_OFFSET
+         else
+            control = new_hot_key_num + REGION_2_OFFSET - CONTROL_BORDER
+         end
+      elseif player_virtual_inventory and control >= REGION_2_START and control <= REGION_2_END then
+         local hot_key_num = control - REGION_2_OFFSET + CONTROL_BORDER
+         local new_hot_key_num = player_virtual_inventory:GetOriginalSlot(hot_key_num)
+
+         if new_hot_key_num <= CONTROL_BORDER then
+            control = new_hot_key_num + REGION_1_OFFSET
+         else
+            control = new_hot_key_num + REGION_2_OFFSET - CONTROL_BORDER
+         end
       end
 
       return OnControl(self, control, down)
